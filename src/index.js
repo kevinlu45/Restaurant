31678bc18cb9570c24a472e02b8d2c779a49a040
Kevin Lu <kevinlu455@gmail.com>
2022-01-10 13:38:48 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './style.css';
-console.log('poop');
+import {loadInitial,loadHome,loadMenu,loadContact} from './home';
+
+loadInitial();
+
+let tab = document.querySelectorAll('button');
+tab.forEach(element => {
+    element.addEventListener('click', switchTab);
+});
+
+function switchTab(){
+    let pressed = this.innerText;
+    let main = document.getElementById('main-content');
+    removeAllChildNodes(main);
+    if (pressed == 'Home'){
+        //load home
+        loadHome();
+    } else if (pressed == 'Menu'){
+        //load menu
+        loadMenu();
+    } else if (pressed == 'Contact'){
+        //load Contacts
+        loadContact();
+    }
+
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
